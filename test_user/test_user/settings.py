397e5bf8654599477b9e35a0bc96787ab182fde1
Artjom <sel_it@tut.by>
2024-10-15 16:48:56 +0300

    connect MongoDB

--- test_user/test_user/settings.py
+++ test_user/test_user/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'djongo',
+    'test_user_app',
 ]
 
 MIDDLEWARE = [
@@ -73,10 +75,19 @@ WSGI_APPLICATION = 'test_user.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'user_database',
+        'HOST': 'localhost',
+        'PORT': 27017,
     }
 }
 
